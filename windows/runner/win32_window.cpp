50bcee59c7ee9cb75f2cb23615766300e2eaf581
CarlosManrique-1397 <carlosdmanriquef@gmail.com>
2026-09-07 21:19:57 -0500

    Replace reinterpret_cast with bit_cast for safety

--- windows/runner/win32_window.cpp
+++ windows/runner/win32_window.cpp
@@ -158,9 +158,9 @@ LRESULT CALLBACK Win32Window::WndProc(HWND const window,
                                       WPARAM const wparam,
                                       LPARAM const lparam) noexcept {
   if (message == WM_NCCREATE) {
-    auto window_struct = reinterpret_cast<CREATESTRUCT*>(lparam);
+    auto window_struct = bit_cast<CREATESTRUCT*>(lparam);
     SetWindowLongPtr(window, GWLP_USERDATA,
-                     reinterpret_cast<LONG_PTR>(window_struct->lpCreateParams));
+                     bit_cast<LONG_PTR>(window_struct->lpCreateParams));
 
     auto that = static_cast<Win32Window*>(window_struct->lpCreateParams);
     EnableFullDpiSupportIfAvailable(window);

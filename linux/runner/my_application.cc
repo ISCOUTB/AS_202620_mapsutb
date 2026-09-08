f61dac3bccaccf19106ac397e47020a32d878ba3
nerlis-otero <nelistalways@gmail.com>
2026-09-08 00:29:09 +0000

    Fix #10: replace redundant GtkHeaderBar* type with auto
    Sonar cpp:S5827 (MAJOR) - el tipo de 'header_bar' ya es evidente por
    el cast GTK_HEADER_BAR(...) del lado derecho; se reemplaza por auto.
    
    Refs #10

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -43,7 +43,7 @@ static void my_application_activate(GApplication* application) {
   }
 #endif
   if (use_header_bar) {
-    GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
+    auto* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
     gtk_header_bar_set_title(header_bar, "mapsutb");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
